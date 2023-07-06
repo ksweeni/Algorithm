023fb02e48408936bc857ad6dd947882227bf1ed
ksweeni <ksween@naver.com>
2023-07-06 12:26:06 +0900

    Create 삼각 달팽이.js
    230706
    LV.2 삼각 달팽이
    
    flatMap메서드는 먼저 매핑함수를 사용해 각 엘리먼트에 대해 map 수행 후, 결과를 새로운 배열로 평탄화한다. 둘을 하나의 메소드로 병합할 때 효율적이다.

---
+++ PROGRAMMERS/LV.2/삼각 달팽이.js
@@ -0,0 +1,22 @@
+function solution(n) {
+ 
+    let answer = Array.from({ length: n }, (_, index) => Array(index + 1).fill(0));
+    let x = -1;
+    let y = 0;
+    let count = 0;
+   
+    while(n>0){
+        for(let i =0;i<n;i++){
+            answer[++x][y] = ++count;
+        }
+        for(let i =0;i<n-1;i++){
+            answer[x][++y] = ++count;
+        }
+        for(let i =0;i<n-2;i++){
+            answer[--x][--y] = ++count;
+        }
+        
+        n-=3;
+}
+    return answer.flatMap(v => v);
+}
