ba64479ddbd548c3c03bd9ac243e09e083977845
ksweeni <ksween@naver.com>
2023-07-17 15:34:27 +0900

    Create 1526.py
    230717
    1526 가장 큰 금민수

---
+++ BOJ/Bronze/1526.py
@@ -0,0 +1,12 @@
+import sys
+N = int(sys.stdin.readline().strip())
+
+while True:
+    flag = True
+    for i in str(N):
+        if i != '4' and i != '7':
+            flag = False
+            N -= 1
+    if flag:
+        print(N)
+        break
