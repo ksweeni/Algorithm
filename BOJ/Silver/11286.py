c4a8b39cd7924815823d2c95d361362cbb161bb8
ksweeni <ksween@naver.com>
2023-08-09 12:19:10 +0900

    Create 11286.py
    230809
    11286 절댓값 힙

---
+++ BOJ/Silver/11286.py
@@ -0,0 +1,15 @@
+import heapq
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+q = []
+for i in range(n):
+  num = int(input())
+  if num == 0 :
+    if q:
+      print(heapq.heappop(q)[1])
+    else:
+      print(0)
+  else :
+    heapq.heappush(q,(abs(num),num))
